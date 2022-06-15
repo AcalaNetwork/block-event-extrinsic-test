cee283e567bdd88b99af6dfbca5775d18fc46cb7
shenger <714275326@qq.com>
2022-06-15 10:34:34 +0800

    first commit

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,6 @@
+import { SubstrateBlock } from '@subql/types'
+
+export async function handleBlock(block: SubstrateBlock): Promise<void> {}
+
+export async function handleEvent(block: SubstrateBlock): Promise<void> {}
+export async function handleCall(block: SubstrateBlock): Promise<void> {}
